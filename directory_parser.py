00869e0a0210da9c1b376d90427ba0e76aa91afb
fourK <innocenthacker84@gmail.com>
2018-09-12 01:03:57 +0530

    initialized directory parser

---
+++ directory_parser.py
@@ -0,0 +1,11 @@
+def directory_list():
+    """ takes directory as input  
+        returns a list of child directories
+    """
+    pass
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
